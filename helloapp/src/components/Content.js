06c282b0f2ba42c548e78971eeb8672075cab1eb
Sangeerththanan <s.sangeerththanan@gmail.com>
2023-10-16 18:29:45 +0530

    initial commit

--- helloapp/src/components/Content.js
+++ helloapp/src/components/Content.js
@@ -3,7 +3,7 @@ export default function Content() {
         <>
         <h3 align='center'>I did this section!</h3>
         <p>Fist clone the project with the help of git bash (git clone https://github.com/Sangeerththanan/3.1Group10.git)</p>
-        <p>Then install node_modules (npm istall)</p>
+        <p>Then install node_modules (npm install)</p>
         <p>Then run your project(npm start) and create your components!</p>
         <p>Niljana.js</p>
         <p>Kapil.js</p>
