cbe14c0a2ed57c3f0fde50328eaf7b190bb6b0aa
Sangeerththanan <s.sangeerththanan@gmail.com>
2023-10-16 13:26:00 +0530

    content.js

--- helloapp/src/App.js
+++ helloapp/src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Content from './components/Content';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <p>Niljana.js</p>
+    <hr></hr>
+    <p>Kapil.js</p>
+    <hr></hr>
+    <p>Lux.js</p>
+    <hr></hr>
+    <p>Naveen.js</p>
+    <hr></hr>
+    <p>Rukku.js</p>
+    <hr></hr>
+    <p>Saja.js</p>
+    <hr></hr>
+    <p>Vish.js</p>
+    <hr></hr>
+    <Content></Content>
+    <p>Niljana.js</p>
+    <p>Kapil.js</p>
+    <p>Lux.js</p>
+    <p>Naveen.js</p>
+    <p>Rukku.js</p>
+    <p>Saja.js</p>
+    <p>Vish.js</p>
+    </>
   );
 }
 
