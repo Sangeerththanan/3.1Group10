866c43076bc989973564e58d15f6e4c3c2a44580
Luxman03 <kunaluxman@gmail.com>
2023-12-07 09:26:47 +0530

    deleteEmployee

---
+++ EmployeeBookingApp/Frontend/app/components/deleteEmployee.js
@@ -0,0 +1,43 @@
+//import libraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Client from '../../api/Client';
+
+// create a component
+const EmployeeDelete = ({ employeeEmail, onDelete }) => {
+    async function handleDelete() {
+        console.log({ employeeEmail })
+        try {
+            await Client.delete(`/employees/${ employeeEmail }`);
+            // Call the onDelete callback after successful deletion
+            onDelete();
+        } catch (error) {
+            console.error('Error deleting employee:', error);
+        }
+    }
+
+    return (
+        <TouchableOpacity style={styles.container}>
+            <Text
+                style={{ fontSize: 18, color: '#fff', padding: 10 }}
+                onPress={handleDelete}
+            >
+                Delete
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        height: 45,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'red'
+    },
+});
+
+//make this component available to the app
+export default DeleteBtn;
