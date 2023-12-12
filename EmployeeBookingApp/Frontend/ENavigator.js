f5cf5b47207926d4bd8080c41bb76d87c13aa1d0
SajathMH <sajathmohamath272@gmail.com>
2023-12-12 11:39:18 +0530

    Employeer navigator

--- EmployeeBookingApp/Frontend/ENavigator.js
+++ EmployeeBookingApp/Frontend/ENavigator.js
@@ -3,24 +3,24 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import WelcomePage from './components/WelcomePage';
-import AppForm from './components/AppForm';
-import { useLogin } from './context/LoginProvider';
-import DrawerNavigator from './DrawerNavigator';
-import LoginForm from './components/LoginForm';
+import AppForm from './components/EmployeerAppForm';
+import { useLogin } from './context/ELoginProvider';
+import DrawerNavigator from './EDNavigator';
+import LoginForm from './components/ELoginForm';
 const Stack = createStackNavigator();
 
 const StackNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen component={WelcomePage} name='WelcomePage' />
-      <Stack.Screen component={AppForm} name='AppForm' />
-     <Stack.Screen component={LoginForm} name='LoginForm' />
+      <Stack.Screen component={AppForm} name='EmployeerAppForm' />
+     <Stack.Screen component={LoginForm} name='ELoginForm' />
     </Stack.Navigator>
   );
 };
 
 const MainNavigator = () => {
   const { isLoggedIn } = useLogin();
-  return isLoggedIn ? <DrawerNavigator /> : <StackNavigator />;
+  return isLoggedIn ? <EDNavigator /> : <StackNavigator />;
 };
 export default MainNavigator;
