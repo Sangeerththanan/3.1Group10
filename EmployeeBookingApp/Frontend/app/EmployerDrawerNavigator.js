de6a35282c197388c47f4566b20525b3daf0976c
nilojana09 <snila7000@gmail.com>
2023-12-19 21:55:55 +0530

    Employment

--- EmployeeBookingApp/Frontend/app/EmployerDrawerNavigator.js
+++ EmployeeBookingApp/Frontend/app/EmployerDrawerNavigator.js
@@ -6,6 +6,7 @@ import Tasks from './components/Tasks';
 import { useLogin } from './context/LoginProvider';
 import EmployerProfile from './components/employer/EmployerProfile';
 import EmployeeComplain from './components/employer/EmployeeComplain';
+import Employment from './components/employee/Employment';
 
 const Drawer = createDrawerNavigator();
 
@@ -54,6 +55,7 @@ const EmployerDrawerNavigator = () => {
   return (
     <Drawer.Navigator drawerContent={(props) => <CustomDrawer {...props} />} >
       <Drawer.Screen component={EmployerProfile} name='Profile' />
+      <Drawer.Screen component={Employment} name='Employment' />
       <Drawer.Screen component={EmployeeComplain} name='Complain' />
       <Drawer.Screen component={Home} name='Home' />
       <Drawer.Screen component={Tasks} name='Tasks' />
