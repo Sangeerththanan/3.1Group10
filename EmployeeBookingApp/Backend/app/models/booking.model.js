8f55efe418c89dd4b465f060a04fa417b8a36f46
nimsaraniv <87105061+nimsaraniv@users.noreply.github.com>
2024-03-16 13:11:32 +0530

    Update booking.model.js

--- EmployeeBookingApp/Backend/app/models/booking.model.js
+++ EmployeeBookingApp/Backend/app/models/booking.model.js
@@ -1,8 +1,10 @@
 module.exports = mongoose => {
     const bookingchema = new mongoose.Schema({
+        ERemail: { type: String , required: true},
         employer: { type: String , required: true},
         type: { type: String , required: true},
         employee: { type: String , required: true},
+        EEemail: { type: String , required: true},
     });
 
 
